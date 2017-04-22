904b977ce60764180b960a466098cecef1317efb
Tatiana <tanyusha_shkoda@mail.ru>
2017-04-22 19:04:14 +0300

    Created empty project

---
+++ ASP.MVC-2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.MVC_2017.Startup))]
+namespace ASP.MVC_2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
